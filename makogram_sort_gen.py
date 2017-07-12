6384bf19ff604adad891770c6ca136ac390a76ae
Michal Young <michal.young@gmail.com>
2017-07-12 14:31:35 -0700

    Repetition limits and weights

---
+++ makogram_sort_gen.py
@@ -0,0 +1,52 @@
+"""
+Generate test cases for the 
+built-in Python function 'sorted', 
+as an example of grammar-based 
+test generation with an oracle built into the 
+test driver. 
+"""
+from makogram.grammar import Grammar
+import genseq
+
+g = Grammar()
+
+
+g.prod("skel", """
+import nose
+
+def ordered(s):
+    if len(s) < 1:
+        return True
+    prior = s[0]
+    for ch in s[1:]:
+        if ch < prior:
+            return False
+        prior = ch
+    return True
+
+${tests()}
+""")
+
+# tests ::=  tests test | test
+g.prod("tests", "${tests()} ${test()}", max_uses=10)
+g.prod("tests", "${test()}", weight=0)
+
+# test ::= (boilerplate with next_name) 
+g.prod("test", """
+input_str = "${next_name()}"
+output_str = sorted(input_str)
+assert len(input_str) == len(output_str)
+assert ordered(output_str)
+""")
+
+#  next_name is a procedural attachment that 
+#  draws names from a fixed sequence
+# 
+names = genseq.names()
+@g.procdef("next_name")
+def next_name():
+    return next(names)
+
+print(g.gen("${skel()}"))
+
+
