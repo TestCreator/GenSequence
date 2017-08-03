41f94ca04ca9dfd1786f3e2afd567cd1c9a9312f
Jamie Zimmerman <jamiez@uoregon.edu>
2017-08-02 19:21:15 -0700

    playing around with closures

---
+++ closure_practice.py
@@ -0,0 +1,31 @@
+#Multiplication practice
+def multiplier_of(num):
+	def mult(n):
+		return n*num
+	return mult
+
+x = multiplier_of(5)
+t = x(4)
+print(t)
+
+#Print Titles
+def print_message(prefix):
+	def edit(name):
+		return prefix + name
+	return edit
+title = print_message("Dr.")
+label = title("Jones")
+print(label + '\n')
+
+#Decorator Practice
+def jazz_it_up(execute):
+	def beautify(guy):
+		print("***")
+		execute(guy)
+		print("***")
+	return beautify
+
+@jazz_it_up
+def print_name(name):
+	print(name)
+print_name("Mr. Business")
