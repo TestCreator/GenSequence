b5b8625284865ccea2582dc1d484d4ea25d6a4a2
Jamie Zimmerman <jamiez@uoregon.edu>
2018-02-08 21:35:50 -0800

    comments and lines

--- preprocessor.py
+++ preprocessor.py
@@ -1,4 +1,13 @@
-import sys
+import sys #to get command line args - the file name we want to compile!
+from range import Range #Range objects are declared, often as globals
+import ast #evaluation of strs into the objects the string represents
+
+def eval_global(strrep):
+        partition = strrep.split(' ')
+        rejoin = partition[0] + " = " + partition[1]
+        res = ast.literal_eval(rejoin) 
+        return res
+
 
 def get_file_name():
         syntax_file_to_read = ""
@@ -7,11 +16,43 @@ def get_file_name():
                         syntax_file_to_read = parm
         return syntax_file_to_read
 
+def scrub_down(sectionalist):
+        clean_section = []
+        for lin in sectionalist:
+                if (lin == "\n" or lin.startswith("#")):
+                        continue
+                ## get rid of comments, and then get rid of ending newline, and tabins. some tabins are read as multiple spaces
+                lin = lin.split("#")[0].strip("\t").strip("\n").strip(' ') 
+                clean_section.append(lin)
+        return clean_section
+
+
 def open_and_print(filename):
-        f = open(filename, 'r')
-        stuff = f.read()
-        print(stuff)
-        f.close()
+        f_in = open(filename, 'r') 
+
+        # the parsing begins
+        ############################
+
+        sections = [[] for _ in range(10)]
+        i = 0
+        line = f_in.readline()
+        while (line != ''):
+                if (not line.startswith("%%%%%%%%%%%%%%%%%%%%")):
+                        sections[i].append(line)
+                else:
+                        i += 1
+                line = f_in.readline()
+
+        for i in range(len(sections)):
+                sections[i] = scrub_down(sections[i])
+        #for section in sections:
+                #print(section)
+        thingy = eval_global(sections[0])
+        print(thingy)
+
+
+        ############################
+        f_in.close()
 
 def main():
         open_and_print(get_file_name())
